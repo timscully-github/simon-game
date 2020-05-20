732a33bbed40ed50d262982cbb6b808aae3ce450
Tim Scully <hello@timscully.co.uk>
2020-05-20 13:43:59 +0100

    make random chosen colour flash on page

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 3);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   console.log(gamePattern);
 }
 

a390f6cba6170ef286b3c7d1ed136ec6cd244314
Tim Scully <hello@timscully.co.uk>
2020-05-20 14:29:00 +0100

    used jquery to created handle function to find id of button pressed

--- game.js
+++ game.js
@@ -2,11 +2,19 @@ var gamePattern = [];
 var buttonColors = ["red","blue","green","yellow"];
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 3);
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
-  console.log(gamePattern);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+
+  $(".btn").click(function() {
+    var userChosenColor = $(this).attr('id');
+    console.log(userChosenColor);
+  });
 }
 
 nextSequence();
